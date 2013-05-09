92b6c07eaa4f86b6cf44fa9909f68ab00bde9522
Sam Henry <samuelhenry@gmail.com>
2013-05-08 19:49:45 -0500

    implemented adding new contact to SimpleDB

--- src/SimpleContacts.java
+++ src/SimpleContacts.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 import com.amazonaws.auth.AWSCredentials;
@@ -10,8 +12,8 @@ import com.amazonaws.services.simpledb.model.Attribute;
 import com.amazonaws.services.simpledb.model.CreateDomainRequest;
 import com.amazonaws.services.simpledb.model.Item;
 import com.amazonaws.services.simpledb.model.ListDomainsResult;
+import com.amazonaws.services.simpledb.model.PutAttributesRequest;
 import com.amazonaws.services.simpledb.model.ReplaceableAttribute;
-import com.amazonaws.services.simpledb.model.ReplaceableItem;
 import com.amazonaws.services.simpledb.model.SelectRequest;
 
 
@@ -24,6 +26,7 @@ public class SimpleContacts {
 	private static Scanner scn = new Scanner(System.in);
 	private static AmazonSimpleDB simpleDBClient;
 	private static String selectedContactId;
+	private static Random random = new Random();
 	
 	public static void main(String[] args) {
 		//welcome the user and give them a chance to edit environment variables before continuing
@@ -46,9 +49,6 @@ public class SimpleContacts {
         for (String domainName : simpleDBClient.listDomains().getDomainNames()) {
             System.out.println("  " + domainName);
         }
-        
-        //put data into my domain
-        //simpleDBClient.batchPutAttributes(new BatchPutAttributesRequest(CONTACT_DOMAIN_TITLE, createSampleData()));
         
 		//formatting
 		System.out.println(LINE_SEPARATOR);
@@ -92,6 +92,10 @@ public class SimpleContacts {
 		return simpleDBClient;
 	}
 
+	/********************************************************************
+	* Make sure the contacts domain exists for our user on SimpleDB.
+	* If not, create it.
+	*********************************************************************/
 	private static void ensureDomainExists() {
 		//Although createDomain() is idemponent, the documentation warns that it
 		//could take up to 10 seconds to create the domain, so we should only call createDomain() once per user
@@ -113,6 +117,9 @@ public class SimpleContacts {
 		}
 	}
 
+	/********************************************************************
+	* Handle user's input for contact manipulation options
+	*********************************************************************/
 	private static void handleUserChoice(String userInput) {
 		//initialize choice to an invalid option
 		int choice = -1;
@@ -162,6 +169,9 @@ public class SimpleContacts {
 		
 	}
 
+	/********************************************************************
+	* List the contacts in the user's contact database
+	*********************************************************************/
 	private static void listContacts() {
         // Select all contacts
         String selectExpression = "select * from `" + CONTACT_DOMAIN_TITLE + "`";
@@ -193,16 +203,22 @@ public class SimpleContacts {
         }
 	}
 	
+	/********************************************************************
+	* Let the user select a contact
+	*********************************************************************/
 	private static void selectContact() {
 		System.out.println("Please enter the Contact ID of the contact you would like to select:");
 		selectedContactId = scn.nextLine();
-		
+		//TODO: verify this is an existing contact
 	}
 	
+	/********************************************************************
+	* Retrieve contact info for the selected contact
+	*********************************************************************/
 	private static void retrieveContactDetails() {
 		// check that a contact has been selected
 		if (selectedContactId == null) {
-			System.out.println("Please select a contact first using option 2");
+			System.out.println("Please select a contact first using this option");
 			return;
 		}
 		
@@ -220,6 +236,9 @@ public class SimpleContacts {
         }
 	}
 	
+	/********************************************************************
+	* Let the user edit a contact's information
+	*********************************************************************/
 	private static void editContactDetails() {
 		// check that a contact has been selected
 		if (selectedContactId == null) {
@@ -229,6 +248,9 @@ public class SimpleContacts {
 		
 	}
 	
+	/********************************************************************
+	* Create a new contact in SimpleDB/S3
+	*********************************************************************/
 	private static void createNewContact() {
 		String first = "",
 				last = "",
@@ -244,17 +266,17 @@ public class SimpleContacts {
 				birthday = "";
 		
 		//lists of phone numbers/labels and email addresses/labels
-		List<String[]> phoneNumbers = new ArrayList<String[]>(),
-			emailAddresses = new ArrayList<String[]>();
+		List<String> phoneRecords = new ArrayList<String>(),
+			emailRecords = new ArrayList<String>();
 		
 		//list of tags
 		List<String> tags = new ArrayList<String>();
 			
 		//get the contact's first name
-		while (first.length() > 0) {
+		do {
 			System.out.println("Enter the contact's first name (mandatory):");
 			first = scn.nextLine();
-		}
+		} while (first.length() == 0);
 		
 		//get the contact's last name
 		System.out.println("Enter the contact's last name (optional - just press enter to skip):");
@@ -266,41 +288,25 @@ public class SimpleContacts {
 			phoneNumber = "";
 			phoneLabel = "";
 			
-			System.out.println("Enter a phone number for this contact (optional - just press enter to skip):");
+			System.out.println("Enter a phone number for this contact with a label (eg. 773-202-5862, Work)");
+			System.out.println("(phone numbers are optional - just press enter to skip):");
 			phoneNumber = scn.nextLine();
 			
-			//get a label for this phone number (or skip if the user is done entering phone numbers)
-			if (phoneNumber.length() > 0) {
-				//require a label for each phone number
-				while(tag.length() == 0) {
-					System.out.println("Enter a label for this phone number (e.g., home, work, cell)");
-					phoneLabel = scn.nextLine();
-				}
-				String[] phoneRecord = {phoneNumber, phoneLabel};
-				phoneNumbers.add(phoneRecord);
-			}
+			if (phoneNumber.length() > 0) phoneRecords.add(phoneNumber);
+			
 		} while (phoneNumber.length() > 0);
 		
 		//collect email addresses/labels
 		do {
 			//reset variables
 			emailAddress = "";
-			emailLabel = "";
 			
-			System.out.println("Enter an email address for this contact (optional - just press enter to skip):");
+			System.out.println("Enter an email address for this contact with a label (eg. samuelh@henrycorp.com, Work)");
+			System.out.println("(email addresses are optional - just press enter to skip):");
 			emailAddress = scn.nextLine();
 			
-			//get a label for this email address (or skip if the user is done entering email addresses)
-			if (emailAddress.length() > 0) {
-				//require a label for each phone number
-				while(tag.length() == 0) {
-					System.out.println("Enter a label for this email address (e.g., personal, work, school)");
-					emailLabel = scn.nextLine();
-				}
-				
-				String[] emailRecord = {emailAddress, emailLabel};
-				phoneNumbers.add(emailRecord);
-			}
+			if (emailAddress.length() > 0) emailRecords.add(emailAddress);
+			
 		} while (emailAddress.length() > 0);
 		
 		//get contact's mailing address
@@ -310,6 +316,10 @@ public class SimpleContacts {
 		//get contact's city
 		System.out.println("Enter the city for this contact (optional - just press enter to skip):");
 		city = scn.nextLine();
+		
+		//get contact's city
+		System.out.println("Enter the state for this contact (optional - just press enter to skip):");
+		state = scn.nextLine();
 		
 		//get contact's zip code
 		System.out.println("Enter the 5 digit zip code for this contact (optional - just press enter to skip):");
@@ -326,69 +336,90 @@ public class SimpleContacts {
 			tag = scn.nextLine();
 			//TODO: add tags to list and reset variable
 		} while (tag.length() > 0);
-		List<ReplaceableItem> sampleData = new ArrayList<ReplaceableItem>();
-    	sampleData.add(new ReplaceableItem("Item_01").withAttributes(
-				new ReplaceableAttribute("First", "Samuel", true),
-				new ReplaceableAttribute("Last", "Henry", true),
-				new ReplaceableAttribute("Tag", "cool", true),
-				new ReplaceableAttribute("Tag", "smart", true),
-				new ReplaceableAttribute("Birthday", "5/18/1984", true)
-				));
 
-		createContactRecordInSimpleDB(first, last, phoneNumbers, emailAddresses, streetAddress, city, state, zip, tags, birthday);
-		createContactPageInS3(first, last, phoneNumbers, emailAddresses, streetAddress, city, state, zip, tags, birthday);
+		//create the contact's database record
+		if (createContactRecordInSimpleDB(first, last, phoneRecords, emailRecords, streetAddress, city, state, zip, tags, birthday)) {
+			//create the contact's S3 page if the record was created correctly
+			createContactPageInS3(first, last, phoneRecords, emailRecords, streetAddress, city, state, zip, tags, birthday);
+		}
 	}
 	
+	/********************************************************************
+	* Create a contact's page in S3
+	*********************************************************************/
 	private static void createContactPageInS3(String first, String last,
-			List<String[]> phoneNumbers, List<String[]> emailAddresses,
+			List<String> phoneRecords, List<String> emailRecords,
 			String streetAddress, String city, String state, String zip,
 			List<String> tags, String birthday) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	private static void createContactRecordInSimpleDB(String first,
-			String last, List<String[]> phoneNumbers,
-			List<String[]> emailAddresses, String streetAddress, String city,
+	/********************************************************************
+	* Create a contact's SimpleDB record
+	*********************************************************************/
+	private static boolean createContactRecordInSimpleDB(String first,
+			String last, List<String> phoneRecords,
+			List<String> emailRecords, String streetAddress, String city,
 			String state, String zip, List<String> tags, String birthday) {
-		List<ReplaceableItem> newContact = new ArrayList<ReplaceableItem>();
-		newContact.add(new ReplaceableItem("Item_01").withAttributes(
-				//add attributes for inputs
-				));
 		
+		if (first.length() == 0) {
+			System.out.println("There was a problem creating this contact. First name is required. Please try again.");
+			return false;
+		}
+		
+		Collection<ReplaceableAttribute> attributes = new ArrayList<ReplaceableAttribute>();
+		
+		//add first name attribute
+		attributes.add(new ReplaceableAttribute("First", first, true));
+		
+		if (last.length() > 0)  attributes.add(new ReplaceableAttribute("Last", last, true)) ;
+		
+		for (String phoneRecord : phoneRecords) {
+			attributes.add(new ReplaceableAttribute("Phone", phoneRecord, true));
+		}
+		
+		for (String emailRecord : emailRecords) {
+			attributes.add(new ReplaceableAttribute("Email", emailRecord, true));
+		}
+		
+		//add street address attribute if it was entered
+		if (streetAddress.length() > 0) attributes.add(new ReplaceableAttribute("Street", streetAddress, true));
+		
+		//add city attribute if it was entered
+		if (city.length() > 0) attributes.add(new ReplaceableAttribute("City", city, true));
+		
+		//add zip attribute if it was entered
+		if (zip.length() > 0) attributes.add(new ReplaceableAttribute("Zip", zip, true));
+		
+		//add state attribute if it was entered
+		if (state.length() > 0) attributes.add(new ReplaceableAttribute("State", state, true));
+		
+		//add tag attributes that were entered
+		for (String tag : tags) attributes.add(new ReplaceableAttribute("Tag", tag, true));
+		
+		//add birthday attribute if it was entered
+		if (birthday.length() > 0) attributes.add(new ReplaceableAttribute("Birthday", birthday, true));
+		
+		//List<ReplaceableItem> contacts = new ArrayList<ReplaceableItem>();
+		
+		try {
+			simpleDBClient.putAttributes(new PutAttributesRequest().withItemName(String.valueOf(random.nextInt(10000))).withAttributes(attributes).withDomainName(CONTACT_DOMAIN_TITLE));
+			return true;
+		} catch (Exception ex) {
+			System.out.println("There was a problem adding your contact to SimpleDB, please try again.");
+			System.out.println(ex.getMessage());
+			return false;
+		}
+
 	}
 
+	/********************************************************************
+	* Let the user search for a contact
+	*********************************************************************/
 	private static void searchContacts() {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	/**
-     * Creates an array of SimpleDB ReplaceableItems populated with sample data.
-     *
-     * @return An array of sample item data.
-     */
-    private static List<ReplaceableItem> createSampleData() {
-    	
-    	List<ReplaceableItem> sampleData = new ArrayList<ReplaceableItem>();
-    	sampleData.add(new ReplaceableItem("Item_01").withAttributes(
-				new ReplaceableAttribute("First", "Samuel", true),
-				new ReplaceableAttribute("Last", "Henry", true),
-				new ReplaceableAttribute("Tag", "cool", true),
-				new ReplaceableAttribute("Tag", "smart", true),
-				new ReplaceableAttribute("Birthday", "5/18/1984", true)
-				));
-    	sampleData.add(new ReplaceableItem("Item_02").withAttributes(
-				new ReplaceableAttribute("First", "Cooper", true),
-				new ReplaceableAttribute("Last", "Henry", true),
-				new ReplaceableAttribute("Tag", "cool", true),
-				new ReplaceableAttribute("Tag", "smart", true),
-				new ReplaceableAttribute("Tag", "handsome", true),
-				new ReplaceableAttribute("Birthday", "6/3/1982", true)
-				));
-		
-
-        return sampleData;
-    }
 }
 
